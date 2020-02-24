55db0523e0878cb6ddb1aa99a7c770253cd6fb1b
tinh0304 <tinhtran0304@yahoo.com>
2020-02-24 17:08:36 -0500

    removed copy statements for copying from another database

--- copy.sh
+++ copy.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
-
-echo COPY team_info FROM jatt
-psql -c "COPY (
-	SELECT ti_id, ti_name, ti_location
-	FROM team_info
-	) TO STDOUT;" jatt | \
-	psql -c "COPY team_info FROM STDIN;" jatt
-
-echo COPY team_data FROM csv
+echo COPY team_data FROM nba_team_stats_00_to_18_edited.csv
 psql -c "\copy team_data FROM nba_team_stats_00_to_18_edited.csv WITH CSV HEADER" jatt
 
-echo COPY player_data FROM csv
+echo COPY player_data FROM Seasons_Stats_edited.csv
 psql -c "\copy player_data FROM Seasons_Stats_edited.csv WITH CSV HEADER" jatt
