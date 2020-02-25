06ead99686af130ba04a5c0aad6afd78f174661c
tinh0304 <tinhtran0304@yahoo.com>
2020-02-24 19:39:03 -0500

    renamed .mssql to .sql And changed teamnames in seasons_stats to corresponding team id

--- copy.sh
+++ copy.sh
@@ -2,5 +2,5 @@
 echo COPY team_data FROM nba_team_stats_00_to_18_edited.csv
 psql -c "\copy team_data FROM nba_team_stats_00_to_18_edited.csv WITH CSV HEADER" jatt
 
-echo COPY player_data FROM Seasons_Stats_edited.csv
-psql -c "\copy player_data FROM Seasons_Stats_edited.csv WITH CSV HEADER" jatt
+echo COPY player_data FROM Seasons_Stats_edited_withID.csv
+psql -c "\copy player_data FROM Seasons_Stats_edited_withID.csv WITH CSV HEADER" jatt
