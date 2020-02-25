2884f186c3990550cb6f09f165b25a2c599f1933
James Mitchell <mitchejp@dukes.jmu.edu>
2020-02-24 20:10:37 -0500

    added basic query indexing

---
+++ index.sql
@@ -0,0 +1,4 @@
+CREATE INDEX ON team_data (t_name, t_season);
+CREATE INDEX ON team_info (ti_name, ti_location);
+CREATE INDEX ON player_info (pi_name, pi_position, pi_college);
+CREATE INDEX ON player_data (p_name, p_pos, p_season);
