b056ffb5466fca3b25ed31daf6ed100f1be92843
tinh0304 <tinhtran0304@yahoo.com>
2020-02-27 11:29:17 -0500

    transfer.sql now correctly transfer teamID and teamName from team_data to team_info

--- schema/transfer.sql
+++ schema/transfer.sql
@@ -1,12 +1,9 @@
 --This is to transfer data from one table to another
 
-INSERT INTO team_info
-SELECT DISTINCT t_id
-FROM team_data;
-
-INSERT INTO team_info
-SELECT DISTINCT t_name
-FROM team_data;
+INSERT INTO team_info(ti_id, ti_name)
+SELECT t_id, t_name
+FROM team_data
+LIMIT 30;
 
 ALTER TABLE team_data
 DROP COLUMN t_name;
