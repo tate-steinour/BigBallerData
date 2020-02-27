2d5a4e8160d1fb7da05cbb487b4989cac0e91494
tate-steinour <steinotw@dukes.jmu.edu>
2020-02-27 11:45:46 -0500

    Finished transfer file for season and player

--- schema/transfer.sql
+++ schema/transfer.sql
@@ -1,4 +1,4 @@
---This is to transfer data from one table to another
+--This is to transfer team data
 
 INSERT INTO team_info(ti_id, ti_name)
 SELECT t_id, t_name
@@ -8,3 +8,16 @@ LIMIT 30;
 ALTER TABLE team_data
 DROP COLUMN t_name;
 
+--This is to transfer season data
+
+INSERT INTO season(s_year)
+SELECT DISTINCT p_season
+FROM player_data;
+
+--This is to transfer player data
+INSERT INTO player_info(pi_id, pi_name, pi_position)
+SELECT p_id, p_name, p_pos
+FROM player_data;
+
+ALTER TABLE player_data
+DROP COLUMN p_name, p_pos;
