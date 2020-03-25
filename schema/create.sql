af52d20e10e8e1ee5025cc3fc30ea20fd8fa14fd
tinh0304 <tinhtran0304@yahoo.com>
2020-03-25 15:17:02 -0400

    changed players' tables to match inteneded schema

--- schema/create.sql
+++ schema/create.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS team_data;
+DROP TABLE IF EXISTS team_stats;
 CREATE TABLE team_stats (
   ts_id integer,
   ts_season text,
@@ -18,36 +18,42 @@ CREATE TABLE team_stats (
   --PRIMARY KEY (t_id, t_season)
 );
 ALTER TABLE team_stats OWNER TO jatt;
-COMMENT ON TABLE team_data IS 'NBA team statistics';
+COMMENT ON TABLE team_stats IS 'NBA team statistics';
 
 
-DROP TABLE IF EXISTS team_info;
+DROP TABLE IF EXISTS team;
 CREATE TABLE team (
   t_id integer PRIMARY KEY,
   t_name text
 );
 ALTER TABLE team OWNER TO jatt;
-COMMENT ON TABLE team_info IS 'information about NBA teams';
+COMMENT ON TABLE team IS 'information about NBA teams';
 
 
-DROP TABLE IF EXISTS player_info;
+DROP TABLE IF EXISTS player;
 CREATE TABLE player (
-  p_id integer PRIMARY KEY,
-  p_name text,
-  p_position text,
+--  p_id integer PRIMARY KEY,
+  p_name text PRIMARY KEY,
   p_yearStart integer,
   p_yearEnd integer,
+  p_position integer,
+  p_height text,
+  p_weight integer,
+  p_birthdate text,
   p_college text
 );
 ALTER TABLE player OWNER TO jatt;
-COMMENT ON TABLE player_info IS 'personal information about NBA players';
+COMMENT ON TABLE player IS 'personal information about NBA players';
 
 
-DROP TABLE IF EXISTS player_data;
+DROP TABLE IF EXISTS player_stats;
 CREATE TABLE player_stats (
   ps_id integer,
+  ps_name text,
   ps_teamid integer,
   ps_season integer,
+  ps_pos text,
+  ps_games integer,
   ps_pts integer,
   ps_fg integer,
   ps_3ptm integer,
@@ -62,5 +68,3 @@ CREATE TABLE player_stats (
 );
 ALTER TABLE player_stats OWNER TO jatt;
 COMMENT ON TABLE player_data IS 'NBA player statistics';
-
-DROP TABLE IF EXISTS season;
