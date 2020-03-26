1f523089cc4e5dd52780f0cd4653673dd16a2481
James Mitchell <mitchejp@dukes.jmu.edu>
2020-03-25 23:26:10 -0400

    modify filenames to align with their view names

---
+++ queries/make_views.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# add environment variables to the path
+export PGHOST=data.cs.jmu.edu
+export PGDATABASE=jatt
+
+#  psql commands
+psql -h data.cs.jmu.edu jatt < three_ptm_wins.sql
+psql -h data.cs.jmu.edu jatt < blocks_by_height.sql
+psql -h data.cs.jmu.edu jatt < max_individual_3ptm.sql
+psql -h data.cs.jmu.edu jatt < players_by_college.sql
+psql -h data.cs.jmu.edu jatt < philly_team_wins.sql
--- queries/philly_team_wins.sql
+++ queries/philly_team_wins.sql
@@ -1,13 +1,13 @@
 --Analyzes the relation between the number of points a team averages in a
 --season and their win total, in this instance we analyze the 76ers
 
-DROP VIEW IF EXISTS philly_team_pts;
-CREATE VIEW philly_team_pts AS
+DROP VIEW IF EXISTS philly_team_wins;
+CREATE VIEW philly_team_wins AS
     SELECT t_name, ts_season, ts_wins
     FROM team_stats
         JOIN team ON t_id=ts_id
     WHERE t_name='Philadelphia 76ers'
     ORDER BY ts_season;
-ALTER VIEW philly_team_pts OWNER TO jatt;
+ALTER VIEW philly_team_wins OWNER TO jatt;
 
 --SELECT * FROM philly_team_pts;
