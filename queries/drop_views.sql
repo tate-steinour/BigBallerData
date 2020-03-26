5ac6909e3bd54128e7bef71e36bf04655c95ec6c
James Mitchell <mitchejp@dukes.jmu.edu>
2020-03-25 20:02:39 -0400

    modified teampts_wins to philly_team_points to better describe its function

--- queries/drop_views.sql
+++ queries/drop_views.sql
@@ -2,4 +2,4 @@ DROP VIEW three_ptm_wins;
 DROP VIEW blocks_by_height;
 DROP VIEW max_individual_3ptm;
 DROP VIEW players_by_college;
-DROP VIEW team_pts_wins;
+DROP VIEW philly_team_pts;
--- queries/teampts_wins.sql
+++
@@ -1,9 +0,0 @@
-DROP VIEW IF EXISTS team_pts_wins;
-CREATE VIEW team_pts_wins AS
-    SELECT t_name, ts_season, ts_wins, ts_pts
-    FROM team_stats
-        JOIN team ON t_id=ts_id
-    WHERE t_name='Philadelphia 76ers'
-    ORDER BY ts_season;
-
-SELECT * FROM team_pts_wins;
