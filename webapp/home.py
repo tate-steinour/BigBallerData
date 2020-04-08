0fb6fd506706d237bdadf418721933cae840277f
Adrian Brazell <brazelam@dukes.jmu.edu>
2020-04-08 12:51:19 -0400

    testing out commit working in environment

--- webapp/home.py
+++ webapp/home.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    con = psycopg2.connect("host=localhost dbname=jatt user=?")
+    con = psycopg2.connect("host=localhost dbname=jatt user=jatt password=3T@R@9D@xcm_5+C+")
+    return render_template('home.html')
 
 
