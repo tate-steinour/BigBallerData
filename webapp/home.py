0e9920d71b6b034ff4f63947ff640fd8780d888d
Adrian Brazell <brazelam@dukes.jmu.edu>
2020-04-08 21:53:55 -0400

    created team_wins_by_season and coresponding function in home.py

--- webapp/home.py
+++ webapp/home.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 
 
-
 @app.route('/')
 def home():
     #con = psycopg2.connect("host=localhost dbname=jatt user=jatt password=3T@R@9D@xcm_5+C+")
     #cursor = con.cursor()
     return render_template('home.html')
+
 
 @app.route('/Blocks_by_height')
 def get_blocks():
@@ -18,9 +18,11 @@ def get_blocks():
     maxHeight = request.args.get("maxHeight", "")
     limit = request.args.get("limit", "")
     if not minHeight and not maxHeight:
-        return render_template('default_blocks_by_height.html') #default when no user input. (what GP3 showed)
+        # default when no user input. (what GP3 showed)
+        return render_template('default_blocks_by_height.html')
     else:
-        con = psycopg2.connect("host=localhost dbname=jatt user=jatt password=3T@R@9D@xcm_5+C+")
+        con = psycopg2.connect(
+            "host=localhost dbname=jatt user=jatt password=3T@R@9D@xcm_5+C+")
         cursor = con.cursor()
         sql = """
         SELECT DISTINCT t_name, ps_season, ps_name, ps_blck, p_height
@@ -30,13 +32,14 @@ def get_blocks():
             JOIN player ON ps_name=p_name
         WHERE ps_blck >= 1
             AND p_height >= %s
-            AND p_height <= %s 
+            AND p_height <= %s
         ORDER BY ps_blck DESC
         LIMIT %s;
         """
         cursor.execute(sql, (minHeight, maxHeight, limit))
         queuryData = cursor.fetchall()
-        return render_template('blocks_by_height.html', queuryData = queuryData)
+        return render_template('blocks_by_height.html', queuryData=queuryData)
+
 
 @app.route('/three_ptm_wins')
 def populate():
@@ -52,10 +55,10 @@ def populate():
     LIMIT %s;
     """
 
-    minWins = request.args.get("minWins","")
-    sStart = request.args.get("sStart","")
-    sEnd   = request.args.get("sEnd","")
-    limit  = request.args.get("limit","")
+    minWins = request.args.get("minWins", "")
+    sStart = request.args.get("sStart", "")
+    sEnd = request.args.get("sEnd", "")
+    limit = request.args.get("limit", "")
     # set defaults
     if minWins == '':
         minWins = 50
@@ -65,8 +68,45 @@ def populate():
         sEnd = '2018'
     if limit == '':
         limit = 10
+    con = psycopg2.connect(
+        "host=localhost dbname=jatt user=jatt password=3T@R@9D@xcm_5+C+")
+    cur = con.cursor()
+    cur.execute(sql, (minWins, sStart, sEnd, limit))
+    dat = cur.fetchall()
+    return render_template("three_ptm_wins.html", dat=dat)
+
+
+@app.route('/team_wins_over_seasons')
+def wins_over_season():
+
+    sql = """
+    SELECT t_name, ts_season, ts_wins
+    FROM team_stats
+        JOIN team ON t_id=ts_id
+    WHERE t_name= %s
+    ORDER BY ts_season
+    LIMIT %s;
+    """
+
+    teamName = request.args.get("teamName", "")
+    limit = request.args.get("limit", "")
+    # set defaulls
+    if teamName == '':
+        teamName = 'Philadelphia 76ers'
+    if limit == '':
+        limit = 10
     con = psycopg2.connect("host=localhost dbname=jatt user=jatt password=3T@R@9D@xcm_5+C+")
     cur = con.cursor()
-    cur.execute(sql, (minWins,sStart,sEnd,limit))
+    cur.execute(sql, (teamName, limit))
     dat = cur.fetchall()
-    return render_template("three_ptm_wins.html",dat = dat)
+    newdat = []
+    for row in dat:
+        newR = []
+        newR.append(row[0])
+        season = row[1]
+        temp = season.split("-")
+        newR.append(temp[0])
+        #newR.append(temp[1])
+        newR.append(row[2])
+        newdat.append(newR)
+    return render_template('team_wins_by_season.html', dat=newdat)
