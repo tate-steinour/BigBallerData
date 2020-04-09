b00b8cdc08e1b079d1c6458a0e78dd2de7ad7a77
tinh0304 <61196263+tinh0304@users.noreply.github.com>
2020-04-08 21:23:19 -0400

    changed blockbyheight to ask for season

--- webapp/home.py
+++ webapp/home.py
@@ -14,10 +14,9 @@ def home():
 
 @app.route('/Blocks_by_height')
 def get_blocks():
-    minHeight = request.args.get("minHeight", "")
-    maxHeight = request.args.get("maxHeight", "")
+    season = request.args.get("season", "")
     limit = request.args.get("limit", "")
-    if not minHeight and not maxHeight:
+    if not season:
         return render_template('default_blocks_by_height.html') #default when no user input. (what GP3 showed)
     else:
         con = psycopg2.connect("host=localhost dbname=jatt user=jatt password=3T@R@9D@xcm_5+C+")
@@ -29,12 +28,11 @@ def get_blocks():
             JOIN team ON t_id=ts_id
             JOIN player ON ps_name=p_name
         WHERE ps_blck >= 1
-            AND p_height >= %s
-            AND p_height <= %s 
+            AND ps_season = %s
         ORDER BY ps_blck DESC
         LIMIT %s;
         """
-        cursor.execute(sql, (minHeight, maxHeight, limit))
+        cursor.execute(sql, (season, limit))
         queuryData = cursor.fetchall()
         return render_template('blocks_by_height.html', queuryData = queuryData)
 
