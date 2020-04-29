76e435c425e65ce9d7a447c5cd78a2f2908ad87a
tate-steinour <steinotw@dukes.jmu.edu>
2020-04-29 17:41:20 -0400

    Added more columns to blocks query to make it fit in a bubble chart

--- queries/blocks_by_height.sql
+++ queries/blocks_by_height.sql
@@ -4,14 +4,14 @@
 
 DROP VIEW IF EXISTS blocks_by_height;
 CREATE VIEW blocks_by_height AS
-    SELECT DISTINCT ps_season, p_height, ROUND(AVG(ps_blck), 2)
+    SELECT DISTINCT ps_season, p_height, ROUND(AVG(p_weight), 2) AS wt, ROUND(AVG(ps_reb), 2) AS reb, ROUND(AVG(ps_blck), 2) AS blk
     FROM player_stats
         JOIN team_stats ON ps_teamid=ts_id
         JOIN team ON t_id=ts_id
         JOIN player ON ps_name=p_name
     WHERE ps_blck >= 1
-    GROUP BY ps_season, p_height
-    ORDER BY ROUND DESC;
+	GROUP BY ps_season, p_height
+    ORDER BY blk DESC, reb DESC;
 ALTER VIEW blocks_by_height OWNER TO jatt;
 
 --SELECT * FROM blocks_by_height;
--- webapp/home.py
+++ webapp/home.py
@@ -68,7 +68,7 @@ def get_blocks():
     SELECT * 
     FROM blocks_by_height
     WHERE ps_season = %s
-    ORDER BY ROUND DESC
+    ORDER BY blk DESC, reb DESC
     LIMIT %s;
     """
     cursor.execute(sql, (season, limit))
